0f4e0aefd676b879719f72fc9e9177cf38045983
evanfrawley <frawley@uw.edu>
2017-11-28 14:30:20 -0800

    go rpc

--- rpc/main.go
+++ rpc/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"net/http"
+	"encoding/json"
+	"log"
+	"net/rpc"
+	"net"
+)
+
 const rpcAddr = "localhost:6000"
 const httpAddr = "localhost:4000"
 
@@ -36,11 +44,44 @@ func newTestSummary(pageURL string) *PageSummary {
 	}
 }
 
+type SummaryService struct {}
+
+func (ss *SummaryService) GetPageSummary(pageURL string, summary *PageSummary) error {
+	*summary = *newTestSummary(pageURL)
+	return nil
+}
+
+func startRPC(addr string) {
+	svc := &SummaryService{}
+	rpc.Register(svc)
+
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("error binding to RPC port: %v\n", err)
+	}
+	log.Printf("RPC server is listening at: %s\n", addr)
+	rpc.Accept(lis)
+}
+
 //TODO: implement an RPC service
 //and an HTTP handler function that
 //both accept a URL and return a PageSummary
 
+func SummaryHandler(w http.ResponseWriter, r *http.Request) {
+	pageSummary := newTestSummary(r.FormValue("url"))
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(pageSummary)
+}
+
 func main() {
 	//TODO: start the RPC server on rpcAddr
 	//and the HTTP server on httpAddr
+
+	go startRPC(rpcAddr)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", SummaryHandler)
+	log.Printf("HTTP server is listening at %s\n", httpAddr)
+	log.Fatal(http.ListenAndServe(httpAddr, mux))
 }
--- rpc/main_test.go
+++ rpc/main_test.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"testing"
+	"fmt"
+	"net/http"
+	"encoding/json"
+	"net/rpc"
+	"log"
 )
 
 //run these benchmarks using:
@@ -11,8 +16,38 @@ const urlToSummarize = "http://example.com"
 
 func BenchmarkRPC(b *testing.B) {
 	//TODO: implement a benchmark for the RPC server
+	client, err := rpc.Dial("tcp", rpcAddr)
+	if err != nil {
+		b.Fatalf("err dialing rpc server: %v\n", err)
+	}
+	defer client.Close()
+
+	psum := &PageSummary{}
+	for i := 0; i < b.N; i++ {
+		if err = client.Call("SummaryService.GetPageSummary", urlToSummarize, psum); err != nil {
+			log.Fatalf("error calling RPC %v\n", err)
+			if psum.URL != urlToSummarize {
+				b.Fatalf("incorrect URL returned. Expected %s but got %s\n", urlToSummarize, psum.URL)
+			}
+		}
+	}
 }
 
 func BenchmarkHTTP(b *testing.B) {
 	//TODO: implement a benchmark for the HTTP server
+	summaryURL := fmt.Sprintf("http://%s?url=%s", httpAddr, urlToSummarize)
+	psum := &PageSummary{}
+	for i := 0; i < b.N; i++ {
+		resp, err := http.Get(summaryURL)
+		if err != nil {
+			b.Fatalf("err getting page summary: %v\n", err)
+		}
+		if err = json.NewDecoder(resp.Body).Decode(psum); err != nil {
+			b.Fatalf("err decoding page summary: %v\n", err)
+		}
+		if psum.URL != urlToSummarize {
+			b.Fatalf("incorrect URL returned. Expected %s but got %s\n", urlToSummarize, psum.URL)
+		}
+		resp.Body.Close()
+	}
 }
